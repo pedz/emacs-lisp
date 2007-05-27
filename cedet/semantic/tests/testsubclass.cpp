4b83a91152085a7ace1f2c25ca2cc03969b187f5
Perry Smith <pedz@easesoftware.com>
2007-05-27 02:37:53 +0000

    Add cedet before compiling

---
+++ cedet/semantic/tests/testsubclass.cpp
@@ -0,0 +1,74 @@
+/* Special test file for Semantic Analyzer and complex C++ inheritance.
+ */
+
+#include <iostream>
+#include "testsubclass.hh"
+
+void animal::moose::setFeet(int numfeet)
+{
+  if (numfeet > 4) {
+    std::cerr << "Why would a moose have more than 4 feet?" << std::endl;
+    return;
+  }
+
+  fFeet = numfeet;
+}
+
+int animal::moose::getFeet()
+{
+  return fFeet;
+}
+
+void animal::moose::doNothing()
+{
+  fFeet = 3;
+}
+
+
+void deer::moose::setAntlers(bool have_antlers)
+{
+  fAntlers = have_antlers;
+}
+
+bool deer::moose::getAntlers()
+{
+  return fAntlers;
+}
+
+void deer::moose::doSomething()
+{
+  // All these functions should be identified by semantic analyzer.
+  getAntlers();
+  setAntlers(true);
+
+  getFeet();
+  setFeet(true);
+
+  doNothing();
+
+  fSomeField = true;
+
+  fIsValid = true;
+}
+
+void deer::alces::setLatin(bool l) {
+  fLatin = l;
+}
+
+bool deer::alces::getLatin() {
+  return fLatin;
+}
+
+void deer::alces::doLatinStuff() {
+  // All these functions should be identified by semantic analyzer.
+  getFeet();
+  setFeet(true);
+
+  getLatin();
+  setLatin(true);
+
+  doNothing();
+
+}
+
+
