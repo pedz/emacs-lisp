02a0b9359caa5a351bcc7c28cefb9611cbd33f97
Perry Smith <pedz@easesoftware.com>
2011-01-22 16:57:36 -0600

    removed Senny's rvm

--- init.el
+++ init.el
@@ -24,6 +24,7 @@ current buffer which is assumed to be *scratch*"
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(ansi-color-for-comint-mode t)
  '(ansi-color-names-vector ["black" "red" "green" "yellow" "blue" "magenta" "darkturquoise" "white"])
  '(case-fold-search nil)
  '(display-time-mail-file (quote none))
